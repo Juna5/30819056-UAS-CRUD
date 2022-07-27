a608e6fad03a1d21a7e1eda8292e12cc2d28434e
juna5 <junaedi.junz38@gmail.com>
2022-07-28 00:02:59 +0700

    first commit

--- android/app/src/main/kotlin/com/uas/uas/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.uas.uas
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/helpers/constants.dart
+++
@@ -1,2 +0,0 @@
-const String URL_SERVER = 'http://192.168.43.131:8000';
-const String path = "/dosen";
